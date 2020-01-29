f42f2867c470a558c4f7d8307727095b143a0a44
mahmud555 <mahmudhossain838@gmail.com>
2020-01-30 00:13:08 +0600

    first_commit

---
+++ subscribe/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from project_AB.settings import EMAIL_HOST_USER
+from .import forms 
+from django.core.mail import send_mail
+    # Create your views here.
+    #DataFlair #Send Email
+def subscribe(request):
+    sub = forms.Subscribe()
+    if request.method == 'POST':
+        sub = forms.Subscribe(request.POST)
+        subject = 'Welcome to DataFlair'
+        message = 'Hope you are enjoying your Django Tutorials'
+        recepient = str(sub['Email'].value())
+        send_mail(subject, message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+        return render(request, 'subscribe/success.html', {'recepient': recepient})
+    return render(request, 'subscribe/index.html', {'form':sub})
